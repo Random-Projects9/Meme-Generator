3be00c7e685e92101b69378786d5e52c821e7463
Aman <amanjaiswal144181@gmail.com>
2025-09-09 22:14:00 +0530

    Added random image pull

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
 function Main() {
-    const [meme,setMeme] = useState({
+    const [meme, setMeme] = useState({
         topText: "One does not simply",
-        bottomText : "Walk into Mordor",
-        imageUrl : "http://i.imgflip.com/1bij.jpg"
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
-
     const [allMemes, setAllMemes] = useState([])
     
     useEffect(() => {
@@ -15,16 +14,22 @@ function Main() {
             .then(data => setAllMemes(data.data.memes))
     }, [])
     
-    function handleChange(event){
-        const {value} = event.currentTarget
-        setMeme(prevMeme =>{
-            return {
-                ...prevMeme,
-                topText : value
-            }
-        })
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageUrl: newMemeUrl
+        }))
     }
-
+    
+    function handleChange(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
 
     return (
         <main>
@@ -48,7 +53,7 @@ function Main() {
                         value={meme.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🏞️</button>
+                <button onClick={getMemeImage}>Get a new meme image 🏞️</button>
             </div>
             <div className='meme'>
                 <img src= {meme.imageUrl} />
